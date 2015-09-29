0bc036bc1db52fb72f362059c9974816b6c36b05
Dragonfire <bastian616-git@yahoo.de>
2015-09-30 01:40:33 +0200

    Use bootstrap Cyborg Theme

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -8,6 +8,11 @@ def index():
 @app.route('/clans')
 def clans():
     return render_template('clans.html')
+
+# TODO: remove this in production mode
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
 
 if __name__ == '__main__':
     app.debug = True
